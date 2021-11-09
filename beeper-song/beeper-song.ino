3fa7cb7d116014b2a95f1e5a4a637c27730a18fa
Francisco Igor <franciscoigor@gmail.com>
2021-11-09 09:50:38 -0800

    Update beeper-song.ino

--- beeper-song/beeper-song.ino
+++ beeper-song/beeper-song.ino
@@ -31,21 +31,21 @@ const int MI = 330;
 const int FA = 349;
 const int SOL= 392;
 const int LA = 440;
+const int SIb = 469;
 const int SI = 493;
 
 int notes[]= {
   DO, RE, MI, FA, SOL, LA, SI
 };
 
+
 int song[] = {
-  DO, DO, RE, RE, MI, MI, DO, DO, 
-  DO, DO, RE, RE, MI, MI, DO, DO,
-  MI, MI, FA, FA, SOL, SOL, SOL, SOL,
-  MI, MI, FA, FA, SOL, SOL, SOL, SOL,
-  SOL, LA, SOL, FA, MI, MI, DO, DO,
-  SOL, LA, SOL, FA, MI, MI, DO, DO,
-  RE, RE, SOL/2, SOL/2, DO, DO, DO, DO, 
-  RE, RE, SOL/2, SOL/2, DO, DO, DO, DO
+  // mary had a little lamb
+  // MI, RE, DO, RE, MI, MI, MI, 0, RE, RE, RE, 0, MI, SOL, SOL, 0, MI, RE, DO, RE, MI, MI, MI, MI, RE, RE, MI, RE, DO, DO, DO, 0
+  // happy birthday
+  // DO, DO, RE, DO, FA, MI, 0 , DO, DO, RE, DO, SOL, FA, 0, DO, DO, DO*2, LA, FA, FA, MI, RE, 0, SIb, SIb, LA, FA, SOL, FA, 0
+  // twinkle twinkle
+  DO, DO, RE, RE, MI, MI, DO, DO, DO, DO, RE, RE, MI, MI, DO, DO, MI, MI, FA, FA, SOL, SOL, SOL, SOL, MI, MI, FA, FA, SOL, SOL, SOL, SOL, SOL, LA, SOL, FA, MI, MI, DO, DO, SOL, LA, SOL, FA, MI, MI, DO, DO, RE, RE, SOL/2, SOL/2, DO, DO, DO, DO, RE, RE, SOL/2, SOL/2, DO, DO, DO, DO, 0
 };
 
 int pos;
@@ -60,14 +60,13 @@ void loop() {
 
    int analorReading = analogRead(ANALOG_IN);
    int velocity = map(analorReading, 0, 1023, 1, 4);
+   size_t size = sizeof(song)/sizeof(song[0]);
    
-   Serial.println(pos);
-   Serial.println(song[pos]);
    tone(BUZZER_OUT, song[pos], 400 / velocity + 10);
    delay(400 / velocity );
 
    pos++;
-   if (pos >= 64){
+   if (pos >= size){
     pos = 0;
    }
   
