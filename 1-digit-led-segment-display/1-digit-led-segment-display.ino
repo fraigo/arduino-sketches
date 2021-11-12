3cb8bb3f9c85d870fbc1d1920be6939af27fca27
Francisco Igor <franciscoigor@gmail.com>
2021-11-11 19:31:17 -0800

    Update 1-digit-led-segment-display.ino

--- 1-digit-led-segment-display/1-digit-led-segment-display.ino
+++ 1-digit-led-segment-display/1-digit-led-segment-display.ino
@@ -10,9 +10,9 @@
  *  - 1-Digit led display (10 pin, pin 3 & 8 common cathode)
  *  - Display Pins 3 and 8 (common cathode) connected to a 1K resistance
  *  - 1k resistance connected to GND
- *  - Pins 1,2,4 connected to D3, D4, D5 (lower display Segments)
- *  - Pins 10, 9, 7, 6 connected to D6, D7, D8, D9 (upper display segments)
- *  - Optionally (not used), pin 6 connected to D10 (decimal point)
+ *  - Pins 1,2,4 connected to D2, D3, D4 (lower display Segments)
+ *  - Pins 10, 9, 7, 6 connected to D5, D6, D7, D8 (upper display segments)
+ *  - Optionally, pin 5 connected to D9 (decimal point)
  *  
  *  Led display pins:
  *  
@@ -33,19 +33,21 @@
  *      *
  */
 int a[10][10]={
-               {0,0,0,1,1,1,0,1,1,1},  //0
-               {0,0,0,0,0,1,0,0,0,1},  //1
-               {0,0,0,1,1,0,1,0,1,1},  //2
-               {0,0,0,0,1,1,1,0,1,1},  //3
-               {0,0,0,0,0,1,1,1,0,1},  //4
-               {0,0,0,0,1,1,1,1,1,0},  //5
-               {0,0,0,1,1,1,1,1,1,0},  //6
-               {0,0,0,0,0,1,0,0,1,1},  //7
-               {0,0,0,1,1,1,1,1,1,1},  //8
-               {0,0,0,0,1,1,1,1,1,1},};//9  
+   {0,1,1,1,0,1,1,1},  //0
+   {0,0,0,1,0,0,0,1},  //1
+   {0,1,1,0,1,0,1,1},  //2
+   {0,0,1,1,1,0,1,1},  //3
+   {0,0,0,1,1,1,0,1},  //4
+   {0,0,1,1,1,1,1,0},  //5
+   {0,1,1,1,1,1,1,0},  //6
+   {0,0,0,1,0,0,1,1},  //7
+   {0,1,1,1,1,1,1,1},  //8
+   {0,0,1,1,1,1,1,1},  //9 
+}; 
+const int DOT = 9;
 void setup(){  
-  for (int i=3;i<=9;i++) {  
-    pinMode(i,OUTPUT);  
+  for (int i=1;i<=7;i++) {  
+    pinMode(i+1,OUTPUT);  
   }  
   Serial.begin(9600);
 }  
@@ -57,20 +59,20 @@ void setup(){
  *  - dly  : delay to apply by segment
  *  - onoff: enabled or disabled mode 
  */
-void printnum(int num, int dly, int onoff){  
-  for (int i=3;i<=9;i++){  
-    digitalWrite(i,a[num][i]*onoff);  
-    if (a[num][i]==1){
-      delay(dly);
-    }
+void printnum(int num, int onoff){  
+  for (int i=1;i<=7;i++){  
+    digitalWrite(i+1,a[num][i]*onoff);  
   }  
 }  
 
 void loop(){  
   for (int i=0;i<=9;i++){  
-    printnum(i, 50, 1);  
-    delay(500); 
-    printnum(i, 30, 0); 
-    delay(500);  
+    printnum(i, 1);  
+    delay(700); 
+    printnum(i, 0); 
+    digitalWrite(DOT,HIGH);
+    delay(200);
+    digitalWrite(DOT,LOW);
+    delay(100);
   }  
-}  
+}
